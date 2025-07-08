9372d650499de54d2029b0d6945d0ae48876c937
Kethelyn Alves <141461344+KethelynAlves@users.noreply.github.com>
2025-07-08 10:00:44 -0300

    Delete CheckList directory

--- CheckList/src/main/resources/tabelaLista.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXIST tarefa(
-    idTarefa INTEGER PRIMARY KEY AUTOINCREMENT,
-    descricao TEXT NOT NULL,
-    data_alteracao TEXT,
-    concluido INTEGER NOT NULL DEFAULT 0
-    );
-)
