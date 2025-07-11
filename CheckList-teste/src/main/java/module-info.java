56fe50f2bed628ee5eaaa17301fec34e9ad8f8cb
Kethelyn Alves <141461344+KethelynAlves@users.noreply.github.com>
2025-07-10 22:59:04 -0300

    Add files via upload

---
+++ CheckList-teste/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.checklist.checklist {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires java.sql;
+
+    opens com.checklist.checklist to javafx.fxml;
+    exports com.checklist.checklist;
+    exports com.checklist.checklist.controllers;
+    opens com.checklist.checklist.controllers to javafx.fxml;
+}
